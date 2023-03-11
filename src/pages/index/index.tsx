df96b880d4235cc31e91786f7a372816553e04f4
刘正午 <598535774@qq.com>
2023-03-12 00:11:18 +0800

    feat: 提交ellipsis报错Demo

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,38 @@
 import { View } from "@tarojs/components";
-import { SearchBar } from "@nutui/nutui-react-taro";
+import { Ellipsis } from "@nutui/nutui-react-taro";
 
 const DemoIndex = () => {
   return (
     <View>
-      <SearchBar placeholder="上京东，购好物" />
+      <View>
+        <View>
+          <View>
+            <View>
+              <View>
+                <View>
+                  <View>
+                    <View>
+                      <View>
+                        <View>
+                          <View>
+                            <View>
+                              <View>
+                                <View>
+                                  <Ellipsis content="NutUI3.0上线后我们研发团队也在不断的优化、测试、使用、迭代 Vue3 的相关组件，但是在跨端小程序的开发过程中，发现没有合适的组件库可以支持多端开发。为了填补这一空白，同时为了优化开发者体验，让 NutUI 能够为更多的开发者带来便利，我们决定在 NutUI 中增加小程序多端适配的能力。" />
+                                </View>
+                              </View>
+                            </View>
+                          </View>
+                        </View>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
