615238f2aaa8cfa7eeb4a2ba511f0e0152928e15
刘正午 <598535774@qq.com>
2023-03-12 00:35:29 +0800

    feat: swipe问题

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import { View } from "@tarojs/components";
-import { SearchBar } from "@nutui/nutui-react-taro";
+import { Cell, Button, Swipe, Dialog } from "@nutui/nutui-react-taro";
+
+import "./index.scss";
 
 const DemoIndex = () => {
+  const [visible1, setVisible1] = useState(false);
   return (
-    <View>
-      <SearchBar placeholder="上京东，购好物" />
+    <View className="demo-index">
+      <Swipe
+        rightAction={
+          <Button
+            type="primary"
+            shape="square"
+            onClick={() => setVisible1(true)}
+          >
+            删除
+          </Button>
+        }
+      >
+        <Cell title="左滑删除" roundRadius={0} />
+        <Dialog
+          title="组件调用"
+          visible={visible1}
+          onOk={() => setVisible1(false)}
+          onCancel={() => setVisible1(false)}
+        >
+          如果需要在弹窗内嵌入组件或其他自定义内容，可以使用组件调用的方式。
+        </Dialog>
+      </Swipe>
     </View>
   );
 };
