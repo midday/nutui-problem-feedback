60d4fa110aed37242b5161828dec360dcf9cd28f
刘正午 <598535774@qq.com>
2023-03-11 23:45:49 +0800

    feat: 初次提交

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import './app.scss'
+
+const App = ({children}) => {
+  return children
+}
+
+export default App
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "@tarojs/components";
+import { SearchBar } from '@nutui/nutui-react-taro';
+
+
+const App = () => {
+  return (
+    <View>
+      <SearchBar placeholder="上京东，购好物" />
+    </View>
+  );
+};
+export default App;
