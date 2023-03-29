4da842f81682d61a68b388fe5bb427ed8d525c82
刘正午 <598535774@qq.com>
2023-03-29 15:30:57 +0800

    feat: 添加时间滚动穿透Demo

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,64 @@
+import React, { useState } from "react";
+import { DatePicker, Cell } from "@nutui/nutui-react-taro";
 import { View } from "@tarojs/components";
-import { SearchBar } from "@nutui/nutui-react-taro";
 
-const DemoIndex = () => {
+const App = () => {
+  const [show1, setShow1] = useState(false);
+  const [desc1, setDesc1] = useState("2012年 01月 01日");
+  const confirm1 = (values: (string | number)[], options: PickerOption[]) => {
+    setDesc1(options.map((option) => option.text).join(" "));
+  };
   return (
-    <View>
-      <SearchBar placeholder="上京东，购好物" />
-    </View>
+    <>
+      <Cell title="显示中文" desc={desc1} onClick={() => setShow1(true)} />
+      <DatePicker
+        title="日期选择"
+        visible={show1}
+        isShowChinese
+        onCloseDatePicker={() => setShow1(false)}
+        onConfirmDatePicker={(values, options) => confirm1(values, options)}
+      />
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+      <View>滚动</View> <View>滚动</View> <View>滚动</View> <View>滚动</View>
+    </>
   );
 };
-export default DemoIndex;
+export default App;
