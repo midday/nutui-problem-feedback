7cb90020b43348229016e7cdb4495c62fe68bd26
刘正午 <598535774@qq.com>
2023-04-03 12:40:46 +0800

    feat: 选项卡paneKey为空串时被识别成了0

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,27 @@
 import { View } from "@tarojs/components";
-import { SearchBar } from "@nutui/nutui-react-taro";
+import { SearchBar, Tabs } from "@nutui/nutui-react-taro";
+import { useState } from "react";
 
 const DemoIndex = () => {
+  const [tab1value, setTab1value] = useState<string>("");
   return (
     <View>
-      <SearchBar placeholder="上京东，购好物" />
+      <Tabs
+        value={tab1value}
+        onChange={({ paneKey }) => {
+          setTab1value(paneKey);
+        }}
+      >
+        <Tabs.TabPane paneKey="" title="全部">
+          全部内容
+        </Tabs.TabPane>
+        <Tabs.TabPane paneKey="PENDING_DELIVERY" title="待发货">
+          待发货
+        </Tabs.TabPane>
+        <Tabs.TabPane paneKey="CANCELED" title="已取消">
+          已取消
+        </Tabs.TabPane>
+      </Tabs>
     </View>
   );
 };
